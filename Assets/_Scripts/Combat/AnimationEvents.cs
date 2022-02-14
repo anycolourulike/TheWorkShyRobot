c6a7e38070eab841d2b295daad3af3c86a922ae3
Bill Molloy <willymolloy@gmail.com>
2022-02-14 16:00:29 +0000

    Combat Scripts

---
+++ Assets/_Scripts/Combat/AnimationEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rambler.Control;
+
+namespace Rambler.Combat
+{
+    public class AnimationEvents : MonoBehaviour
+    {
+        [SerializeField] Animator anim;
+        [SerializeField] Fighter fighter;
+        [SerializeField] PlayerController playerController;       
+      
+        public void EquipUnarmed()
+        {
+            if(playerController.isHolstered == true)
+            {                
+              fighter.EquipUnarmed();              
+              playerController.isHolstered = false;
+            }  
+            return;          
+        }
+    }
+}
---
+++ Assets/_Scripts/HealthRegenScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rambler.Core;
+using Rambler.Movement;
+using Rambler.Combat;
+
+public class HealthRegenScript : MonoBehaviour
+{
+    [SerializeField] GameObject healthRegenFX;
+    PlayerVitals playerVitals;
+    Health playerHealth;    
+    Mover mover;    
+    
+   void OnTriggerEnter(Collider other) 
+   {
+       if(other.gameObject.tag == "Player")
+       {
+           playerHealth = other.GetComponent<Health>();
+           //var curHP = playerHealth.HealthPoints;
+           //if (curHP == 100f) return;
+
+           healthRegenFX.SetActive(true);
+           playerVitals = other.GetComponent<PlayerVitals>();                           
+           mover = other.GetComponent<Mover>();         
+           mover.Cancel();
+           playerVitals.Restore();                      
+       }     
+   }
+
+   void OnTriggerExit(Collider other) 
+   {
+       if(other.gameObject.tag == "Player")
+       {
+           healthRegenFX.SetActive(false);
+       }
+   }
+}
