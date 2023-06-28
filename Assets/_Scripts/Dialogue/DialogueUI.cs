9a21695b7561923e8ee8516c71861c51675743c2
Bill Molloy <willymolloy@gmail.com>
2023-06-29 00:23:01 +0100

    Dialogue Bug Fix

--- Assets/_Scripts/Dialogue/DialogueUI.cs
+++ Assets/_Scripts/Dialogue/DialogueUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcdadb81d98915a183a3b3f3d955e4e4389edf4bd366bbc0a6f4307c3afeed68
-size 2526
+oid sha256:7b4eae7286cc6ce89b149461a101e56bddb6480bf4e5d5f052f708904bee097e
+size 2417
