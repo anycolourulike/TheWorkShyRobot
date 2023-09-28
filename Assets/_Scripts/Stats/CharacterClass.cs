5f987fd4bbe6a07ae25ee13ceddf761299182de2
Bill Molloy <willymolloy@gmail.com>
2023-09-28 12:20:26 +0100

    Hayes Char Set Up

--- Assets/_Scripts/Stats/CharacterClass.cs
+++ Assets/_Scripts/Stats/CharacterClass.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99109d45d28b07bd30a7adc96d1d069327aba30c59aeddfc4356016607bc5261
-size 216
+oid sha256:a71cc37077942b011f1107b1e1cd866060b7f66776f55c07980ca8e6a3cbbf6c
+size 238
