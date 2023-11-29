bddab2d05b8fb1dd014919ec7c55ac5e44efa79f
Bill Molloy <willymolloy@gmail.com>
2023-11-29 21:23:02 +0000

    F2 Update

---
+++ Assets/DoorCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e6a741e9542227a28d81b337a6b1063f9170f0ec7742be6717db44241367f03
+size 1539
--- Assets/_Scripts/Control/CameraManager.cs
+++ Assets/_Scripts/Control/CameraManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ee041981819e1aa7e6f078be568c74d1d6fa9142c9b90046061c3929f0ae0da
-size 1126
+oid sha256:5698e072ac7483746e6171f69cb12f2ca0d2c086d622fb55f2bb777beace0605
+size 1167
