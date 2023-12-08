d4cfad5ddd47d3eebb0902e3ecc497fd43075636
Bill Molloy <willymolloy@gmail.com>
2023-12-08 22:11:53 +0000

    Camera System Update - Cinemachine

--- Assets/DoorCamera.cs
+++ Assets/DoorCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd2daca7b7ff8c8550a2b12bb98d1252e29cc17fdd96eefbf3ca472e5ddebc23
-size 1533
+oid sha256:97573e82396ff6d23c19d97e7cf168f33c6e116f0b2d5ca635359d4dc1cf3989
+size 1514
